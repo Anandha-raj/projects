703f1b83a248bd212c364444a3df2dafd829eb3f
Anandharaj <anandharaj111097@gmail.com>
2024-11-11 23:47:17 +0530

    Done the react router task.

---
+++ reactroutertask/src/components/FullStack.js
@@ -0,0 +1,22 @@
+function FullStack({data, getItems}){
+
+    const filteredData = getItems("fullstack");
+
+    return(
+        <div className="container" style={{alignItems:"center"}}>
+            <div className="row mx-auto">
+                {filteredData.map((item) => (
+                    <div key={item.id} className="col-sm-3 mt-3 mx-auto">
+                        <div className="card" style={{width: "18rem"}}>
+                            <img className="card-img-top" src={item.image} alt="Card image cap" />
+                            <div className="card-body">
+                            <h5 className="card-title">${item.title}</h5>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default FullStack;
